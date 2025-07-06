8d3d4d3bdaf117818af02f6e588da35e4e155e04
Naidu-39 <naidumunjeti36@gmail.com>
2025-07-06 14:23:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask
 import boto3
 from app.routes import bp as routes_bp
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 app.register_blueprint(routes_bp)
 
 # AWS Configuration
 AWS_REGION = 'us-east-1'  # Change to your region
-DYNAMODB_TABLE = 'MovieMagic_Users' # Change to your table name
-SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:545009839820:MovieMagic:47b1832c-94ba-4e81-8813-7bb4fbb603a8' # Change to your SNS topic ARN
+DYNAMODB_TABLE = 'Movies' # Change to your table name
+SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:123456789012:YourTopic' # Change to your SNS topic ARN
 
 # Initialize AWS clients
 dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
@@ -20,5 +20,5 @@ app.config['DYNAMODB_TABLE'] = dynamodb.Table(DYNAMODB_TABLE)
 app.config['SNS_CLIENT'] = sns
 app.config['SNS_TOPIC_ARN'] = SNS_TOPIC_ARN
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
